40090bb2040c0da7a1937e135a8724d072e430e6
kamaal111 <kamaal.f1@gmail.com>
2021-10-10 21:37:12 +0200

    Modifying build number in info.plist

--- Sources/version-bumper/VersionBumper.swift
+++ Sources/version-bumper/VersionBumper.swift
@@ -16,7 +16,35 @@ struct VersionBumper: ParsableCommand {
     @Option(name: .shortAndLong, help: "Info.plist path")
     var infoPlist: String
 
+    enum Errors: Error {
+        case invalidInfoPlistURLError
+        case contentNotFound
+        case buildTagNotFound
+    }
+
     func run() throws {
-        print("hello", buildNumber, infoPlist)
+        try timeFunction {
+            guard let infoPlistURL = URL(string: infoPlist) else { throw Errors.invalidInfoPlistURLError }
+            guard let infoPlistDictionary = NSMutableDictionary(contentsOfFile: infoPlistURL.path) else {
+                throw Errors.contentNotFound
+            }
+            let buildNumberKey = "CFBundleVersion"
+            guard infoPlistDictionary[buildNumberKey] != nil else {
+                throw Errors.buildTagNotFound
+            }
+
+            infoPlistDictionary.setValue("\(buildNumber)", forKey: buildNumberKey)
+
+            infoPlistDictionary.write(toFile: infoPlistURL.path, atomically: false)
+        }
+    }
+
+    private func timeFunction(completion: (() throws -> Void)) rethrows {
+        let start = CFAbsoluteTimeGetCurrent()
+
+        try completion()
+
+        let diff = CFAbsoluteTimeGetCurrent() - start
+        print("Took \(diff) seconds ✨")
     }
 }
